857dd30ed7c75386269410c0f5fd7e62eff5a2ee
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2019-10-09 13:19:19 +0400

    архитектура готова

--- mainwindow.py
+++ mainwindow.py
@@ -4,7 +4,19 @@ from PyQt5.QtCore import *
 
 from mainwindow_ui import Ui_MainWindow
 
+import matplotlib
+matplotlib.use('QT5Agg')
+
+import matplotlib.pylab as plt
+
+
 def setPlotOnWidget(plot, widget):
+    from matplotlib.backends.backend_qt5agg import FigureCanvasQT
+    # from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+    # self.plotWidget = FigureCanvas(fig)
+    # lay = QtWidgets.QVBoxLayout(self.content_plot)
+    # lay.setContentsMargins(0, 0, 0, 0)
+    # lay.addWidget(self.plotWidget)
     pass
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -12,5 +24,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__()
         self.setupUi(self)
 
+    def getDataFromUI(self):
+        pass
+
+    def modellingSMO(self):
+        pass
+
+    def showPlot(self):
+        pass
 
 
