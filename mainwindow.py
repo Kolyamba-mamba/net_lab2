27fa1d5fbaa4d6a98a4243968fe67750d78eb950
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2019-10-09 13:12:28 +0400

    add func

--- mainwindow.py
+++ mainwindow.py
@@ -4,13 +4,13 @@ from PyQt5.QtCore import *
 
 from mainwindow_ui import Ui_MainWindow
 
-def setIconToLabel(label, icon):
-    pxm = QPixmap(f'./img/{icon}.png')
-    label.setPixmap(pxm)
-
+def setPlotOnWidget(plot, widget):
+    pass
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
+
+
 
