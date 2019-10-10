2433703b4fef60bc0d6bce0cc407ec95a5a4304b
tankistqazwsx <alexei.bezruchenko@yandex.ru>
2019-10-10 10:47:23 +0400

    поддержка многоканальности

--- mainwindow.py
+++ mainwindow.py
@@ -10,6 +10,7 @@ matplotlib.use('QT5Agg')
 
 import matplotlib.pylab as plt
 import numpy as np
+from collections import deque
 
 
 def addPlotToLayout(plot, layout: QLayout):
@@ -44,9 +45,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         return np.random.exponential(scale)
 
     def modellingSMO(self, input):
-        queueSize = 0
         currentTime = 0
-        requestsGot = 0
         # в начале ни одной заявки нет
         statGot = {'x':[0], 'y':[0]}
         statDone = {'x':[0], 'y':[0]}
@@ -55,30 +54,59 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         curGot = 0
         curDone = 0
         curRefused = 0
-        curQueue = 0
-        timeNew = self.random(input["input_stream"]) # время, когда придёт новая заявка
-        timeDone = None # время, когда текущая заявка будет сделана (None, если заявок пока нет)
+        timeNew = self.random(input["input_stream"]) # время, когда придёт новая заявка (можно заменить на 0)
 
-        while (requestsGot<input["count_requests"]):
-            if (timeDone != None and timeDone <= timeNew): # обслужена очередная заявка
-                currentTime = timeDone # переставляем время на время выполнения заявки
+        # очередь заявок
+        # структура:
+        # q(["name":'t1', "got":0],["name":'t2', "got":2])
+        queue = deque() #append, popleft, count
+
+        # текущее состояние каналов
+        # структура channels:
+        # { 0: {"name":'t1', "got": 0, "start":0, "end": 7},
+        #   1: {"name":'t2', "got": 2, "start":2, "end": 11} }
+        # вместо внутреннего словаря будет None, если канал простаивает
+        channels = {key: None for key in range(input["channels"])}
+
+        # статистика по каналам
+        # структура statWorkflow:
+        #{ -1: [{"name":'t4', "got": 6}]
+        #   0: [{"name":'t1', "got": 0, "start":0, "end": 7}, {"name": 't3', "got": 5, "start": 7, "end":14}],
+        #   1: [{"name":'t2', "got": 2, "start":2, "end": 11}]  }
+        # name — название заявки
+        # got — время её получения
+        # start — время начала её выполнения
+        # end — время конца выполнения
+        # ключи списка — номера каналов, -1 — канал отброшенных заявок
+        statWorkflow = {key:[] for key in range(-1, input["channels"])}
+        
+
+        while (curGot<input["count_requests"]):
+            min = None
+            # находим наиболее быстро обслуженную заявку и сравниваем с временем поступления новой
+            for ch in channels: 
+                if ((channels[ch] != None) and (min==None or channels[ch]["time"]<timeNew or channels[ch]["time"]<min)):
+                    min = ch
+
+            if (min!=None): # обслужена очередная заявка
+                currentTime = channels[min]["end"] # переставляем время на время выполнения заявки
+                statWorkflow[min].append(channels[min])
                 statDone["x"].append(currentTime)   # добавляем точку на график
                 statDone["y"].append(curDone)
                 curDone+=1
                 statDone["x"].append(currentTime)
                 statDone["y"].append(curDone)
-                if (queueSize==0):
-                    timeDone = None # если очередь пуста, то никто не обслуживается и времени обслуживания нет
-                else:
-                    queueSize-=1 # в ином случае выполняем следующую заявку
-                    statQueue["x"].append(currentTime)   # добавляем точку на график
-                    statQueue["y"].append(curQueue)
-                    curQueue -= 1
-                    statQueue["x"].append(currentTime)
-                    statQueue["y"].append(curQueue)
+                if (queue.__len__()==0):
+                    channels[min] = None
+                else: # в ином случае выполняем следующую заявку
+                    r = queue.popleft()
                     timeDone = currentTime + self.random(input["work_stream"])
+                    channels[min] = {"name":r.name, "got": r.got, "start":currentTime, "end": timeDone}
+                    statQueue["x"].append(currentTime)   # добавляем точку на график
+                    statQueue["y"].append(queue.count()+1)
+                    statQueue["x"].append(currentTime)
+                    statQueue["y"].append(queue.count())
             else: # прибыла новая заявка
-                requestsGot+=1
                 currentTime = timeNew
                 timeNew = currentTime + self.random(input["input_stream"])
                 statGot["x"].append(currentTime)   # добавляем точку на график
@@ -86,17 +114,23 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 curGot+=1
                 statGot["x"].append(currentTime)
                 statGot["y"].append(curGot)
-                if (timeDone==None): # если канал свободен, обслуживаем заявку сразу
+                freeChannel = None
+                for ch in channels:
+                    if (channels[ch]==None):
+                        freeChannel = ch
+                        break
+                if (freeChannel != None): # если канал свободен, обслуживаем заявку сразу
                     timeDone = currentTime + self.random(input["work_stream"])
+                    channels[ch] = {"name":'t'+str(curGot), "got": currentTime, "start":currentTime, "end": timeDone}            
                 else: # иначе пытаемся добавить в очередь
-                    if (queueSize<input["queue_length"]):
-                        queueSize+=1
+                    if (queue.__len__()<input["queue_length"]):
+                        queue.append({"name":'t'+str(curGot), "got": currentTime})
                         statQueue["x"].append(currentTime)   # добавляем точку на график
-                        statQueue["y"].append(curQueue)
-                        curQueue+=1
+                        statQueue["y"].append(queue.__len__()-1)
                         statQueue["x"].append(currentTime)
-                        statQueue["y"].append(curQueue)
+                        statQueue["y"].append(queue.__len__())
                     else:
+                        statWorkflow[-1].append({"name":'t'+str(curGot), "got":currentTime})
                         statRefused["x"].append(currentTime)   # добавляем точку на график
                         statRefused["y"].append(curRefused)
                         curRefused += 1
@@ -111,8 +145,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         statRefused["x"].append(currentTime)   
         statRefused["y"].append(curRefused)
         statQueue["x"].append(currentTime)
-        statQueue["y"].append(curQueue)
-        return [statGot, statDone, statRefused, statQueue]
+        statQueue["y"].append(queue.__len__())
+        return [statGot, statDone, statRefused, statQueue, statWorkflow]
                 
     def calculate_SLOT(self):
         # print(input)
