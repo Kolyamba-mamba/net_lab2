da25bd7b11e7775848c3ba2f7e4dd3e2ed3145f3
tankistqazwsx <alexei.bezruchenko@yandex.ru>
2020-01-11 23:08:09 +0400

    почему работа идёт так медленно

--- modeling/WFQ.py
+++ modeling/WFQ.py
@@ -2,7 +2,7 @@ import numpy as np
 from collections import deque
 from modeling.helper import *
 
-def WFQ(input_stream, count_channels, work_stream, queue_length, count_requests, discipline, **kwargs):
+def WFQ(input_stream, count_channels, work_stream, queue_length, count_requests, discipline, weights_dict, **kwargs):
     currentTime = 0
     statGot = {'x':[0], 'y':[0]}
     statDone = {'x':[0], 'y':[0]}
@@ -12,13 +12,17 @@ def WFQ(input_stream, count_channels, work_stream, queue_length, count_requests,
     curDone = 0
     curRefused = 0
 
+    timeNew_arr = {key: random(work_stream) for key in range(count_channels)}
+    timeNew_arr[0] = 0
+
     timeNew = 0 # время, когда придёт новая заявка
+    numNew = 0 # номер канала, с которого придёт заявка
     timeDone = None # время, когда будет обслужена следующая заявка
     nameDone = None # имя заявки, которую обслужат следующей
 
     # очередь заявок
     # структура:
-    # q(["name":'t1', "got":0],["name":'t2', "got":2])
+    # q(["name":'t1', "got":0, "channel":0],["name":'t2', "got":2, "channel":1])
     queue = []
 
     # текущее состояние каналов
@@ -60,17 +64,32 @@ def WFQ(input_stream, count_channels, work_stream, queue_length, count_requests,
     channels_gps = {key: None for key in range(count_channels)}
 
     while (curGot<count_requests):
-        if (timeNew<=timeDone):
-            
-            # находим первую ненулевую заявку в GPS
-            not_empty = False
-            min_end_time = {}
-            for el in channels_gps:
-                if el!= {}:
-                    not_empty = True
-                    min_end_time = el
-                    break
+        if (timeDone==None or timeNew<timeDone):
+             timeNew_arr[numNew] = random(input_stream)
+            if channels_gps[numNew]==None:
+                # добавляем сразу на обработку
+                channels_gps.append({"name":'t'+str(curGot), "got":currentTime, "channel":numNew})
+                # TODO: пересчитать сроки окончания для gps
+                #weights_dict
+            else:
+                # добавляем в общую очередь
+                queue.append({"name":'t'+str(curGot), "got":currentTime, "channel":numNew})
+                timeNew = random(work_stream)
 
-        if (not_empty):
-            # находим наиболее быстро обслуженную заявку по GPS
-            pass
+                for el in timeNew_arr:
+                if timeNew_arr[el]<timeNew:
+                    timeNew=timeNew_arr[el]
+                    numNew = el
+
+        else:
+
+
+            # находим заявку в GPS с наименьшим временем конца
+            min_end_el = channels_gps[0]
+            for el in channels_gps:
+                if channels_gps["end"]<min_end_el["min"]:
+                    min_end_el = el
+            
+                    
+            
+                
