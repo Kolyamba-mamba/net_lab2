461a2aa9ab1c456bd4ef69edb57e42ec306937fc
tankistqazwsx <alexei.bezruchenko@yandex.ru>
2020-01-11 15:07:44 +0400

    подправил батник

--- run.bat
+++ run.bat
@@ -1,2 +1,4 @@
-python -m PyQt5.uic.pyuic -x mainwindow.ui -o mainwindow_ui.py
-python main.py
+@echo python -m PyQt5.uic.pyuic -x mainwindow.ui -o mainwindow_ui.py > log.txt
+python -m PyQt5.uic.pyuic -x mainwindow.ui -o mainwindow_ui.py >> log.txt 2>>&1
+@echo python main.py >> log.txt
+python main.py >> log.txt 2>>&1
