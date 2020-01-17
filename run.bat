86ca49828a0b128977e3f5669ff9d644c7837ab1
tankistqazwsx <alexei.bezruchenko@yandex.ru>
2020-01-17 11:48:23 +0400

    некит, спешл фо ю

--- run.bat
+++ run.bat
@@ -1,4 +1,3 @@
-@echo python -m PyQt5.uic.pyuic -x mainwindow.ui -o mainwindow_ui.py > log.txt
-python -m PyQt5.uic.pyuic -x mainwindow.ui -o mainwindow_ui.py >> log.txt 2>>&1
-@echo python main.py >> log.txt
-python main.py >> log.txt 2>>&1
+python -m PyQt5.uic.pyuic -x mainwindow.ui -o mainwindow_ui.py
+python main.py
+pause
